9225e639848ca65725dddf4d9b5a694375803094
Authmillenon <authmill@datalove.me>
2011-03-29 02:55:34 +0200

    config.py reintroduced

--- config.py
+++ config.py
@@ -6,17 +6,17 @@
 #  No Copyright, no license, comes as it is.
 
 ## @brief The Database Management System, like MySQL, SQLLite, Postgres, ...
-db_engine		= 'mysql'
+db_engine		= '<database>'
 
 ## @brief The name of the database. 
 #  The database need to have all tables defined as in datalovers_db.sql.
-db_name			= 'datalovers'
+db_name			= '<databasename>'
 
 ##	@brief The username to connect with the database.
-db_username		= 'authmillenon'
+db_username		= '<username>'
 
 ##	@brief The password to connect with the database.
-db_password		= '6fjmdrrhwdRpyGH0'
+db_password		= '<password>'
 
 ##	@brief The hosts URL for references.
-host_url		= 'http://0.0.0.0:8080'
+host_url		= '<host_url>'

2cdfd995a3018268a7ea706c40a923643ea80baa
Phillip Thelen <phillip.thelen@junge-piraten.de>
2011-03-30 18:09:38 +0200

    updated sql file

--- datalovers_db.sql
+++ datalovers_db.sql
@@ -1,6 +1,20 @@
 DROP DATABASE IF EXISTS datalovers;
 CREATE DATABASE IF NOT EXISTS datalovers;
 
+CREATE TABLE IF NOT EXISTS datalovers.sessions (
+    session_id	CHAR(128) NOT NULL,
+    atime 		TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data 		TEXT,
+    UNIQUE INDEX (session_id ASC),
+    PRIMARY KEY (session_id)
+);
+
+CREATE TABLE IF NOT EXISTS `history` (
+  `sender` varchar(23) NOT NULL,
+  `recipient` varchar(23) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
 CREATE TABLE IF NOT EXISTS datalovers.users (
 	nickname		VARCHAR(23) NOT NULL,
 	password		CHAR(64) NOT NULL,
@@ -20,10 +34,3 @@ CREATE TABLE IF NOT EXISTS datalovers.users (
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS datalovers.sessions (
-    session_id	CHAR(128) NOT NULL,
-    atime 		TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    data 		TEXT,
-    UNIQUE INDEX (session_id ASC),
-    PRIMARY KEY (session_id)
-);

d6cd9c31b14ddf9dbf4d5bf7d2d5e352b7281621
authmillenon <authmill@datalove.me>
2011-04-03 14:07:30 +0200

    adding options to log

--- log.py
+++ log.py
@@ -1,9 +1,11 @@
 import logging
 import logging.handlers
+import web
 
 LOG_FILENAME = 'give_datalove.log'
 LOG_LEVEL = logging.DEBUG
-FORMAT = "%(levelname)s (%(module)s.py#%(funcName)s) [%(asctime)s]: %(message)s"
+FORMAT = "%(levelname)s (%(module)s.py) [%(asctime)s]: " + \
+            "%(message)s"
 
 ## The log object
 log = logging.getLogger('MainLog')
@@ -12,7 +14,7 @@ log.setLevel(LOG_LEVEL)
 formatter = logging.Formatter(FORMAT)
 handler = logging.handlers.RotatingFileHandler(
         LOG_FILENAME, 
-        maxBytes=20*1024, 
+        maxBytes=100*1024, 
         backupCount=5
     )
 handler.setFormatter(formatter)

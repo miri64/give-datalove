1e735752a517af19c709cf76315b1ebd79d2d2a2
Authmillenon <authmill@datalove.me>
2012-02-29 14:28:26 +0100

    [model.py] A little bit of order.

--- datalove/give/models.py
+++ datalove/give/models.py
@@ -3,19 +3,60 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 class DataloveProfile(models.Model):
-    user = models.OneToOneField(User,related_name='datalove')
-    available_love = models.PositiveIntegerField(blank=False, null=False, default=settings.DEFAULT_STARTING_DATALOVE)
-    received_love = models.PositiveIntegerField(blank=False, null=False, default=0)
+    user = models.OneToOneField(
+            User, 
+            related_name='datalove', 
+            blank=False, null=False
+        )
+    available_love = models.PositiveIntegerField(
+            blank=False, 
+            null=False, 
+            default=settings.DEFAULT_STARTING_DATALOVE
+        )
+    received_love = models.PositiveIntegerField(
+            blank=False, 
+            null=False, 
+            default=0
+        )
 
 class DataloveHistory(models.Model):
-    sender = models.ForeignKey(DataloveProfile, related_name='send_history')
-    recipient = models.ForeignKey(DataloveProfile, related_name='receive_history')
-    amount = models.PositiveIntegerField(blank=False, null=False)
+    sender = models.ForeignKey(
+            DataloveProfile, 
+            related_name='send_history', 
+            blank=False, 
+            null=False
+        )
+    recipient = models.ForeignKey(
+            DataloveProfile, 
+            related_name='receive_history', 
+            blank=False, 
+            null=False
+        )
+    amount = models.PositiveIntegerField(
+            blank=False, 
+            null=False
+        )
 
 class UserWebsite(models.Model):
-    user = models.ForeignKey(DataloveProfile,related_name='websites')
-    website = models.URLField(max_length=50, blank=False, null=False)
+    user = models.ForeignKey(
+            DataloveProfile, 
+            related_name='websites', 
+            blank=False, 
+            null=False
+        )
+    website = models.URLField(
+            max_length=50, 
+            blank=False, 
+            null=False
+        )
 
 class LovableObject(models.Model):
-    creator = models.ForeignKey(DataloveProfile,related_name='lovable_objects')
-    received_love = models.PositiveIntegerField(blank=False, null=False, default=0)
+    creator = models.ForeignKey(
+            DataloveProfile,
+            related_name='lovable_objects'
+        )
+    received_love = models.PositiveIntegerField(
+            blank=False, 
+            null=False, 
+            default=0
+        )

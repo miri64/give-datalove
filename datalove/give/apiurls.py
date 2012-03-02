9789a5097720b3a594d93f004ad26851a67633f0
Authmillenon <authmill@datalove.me>
2012-03-02 16:57:21 +0100

    [apiurls.py] Format patterns for json and xml (xml format to be implemented)

--- datalove/give/apiurls.py
+++ datalove/give/apiurls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 import views
 
 urlpatterns = patterns('datalove.give.views.api',
-    url('^user/(?P<user_id>\d+)','profile',name='api__profile'),
+    url('^user/(?P<user_id>\d+)$', 'profile', name='api__profile'),
+    url('^(?P<format>(json|xml))/user/(?P<user_id>\d+)$', 'profile'),
 )

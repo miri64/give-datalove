8446660d54adf518e8c3345fbaff42b2910af9f2
Authmillenon <authmill@datalove.me>
2012-03-04 02:16:09 +0100

    [views/urls.py] Add reset password functionality

--- datalove/give/urls.py
+++ datalove/give/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login, logout, password_reset, \
+        password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = patterns('datalove.give.views.html',
     url(r'^$','index'),
@@ -11,6 +12,31 @@ urlpatterns = patterns('datalove.give.views.html',
         ),
     url(r'^logout$',logout,{'next_page': '/'}),
     url(r'^manage_account$','manage_account',name='manage_account'),
+    url(
+            r'^reset_password$',
+            password_reset,
+            {
+                'template_name': 'give/reset_password.html',
+                'email_template_name': 'give/reset_password_email.html',
+                'from_email': 'password-reset@give.datalove.me'
+            }
+        ),
+    url(
+            '^reset_password/done$',
+            password_reset_done,
+            {'template_name': 'give/reset_password_done.html'}
+        ),
+    url(
+            r'^reset_password/(?P<uidb36>.*)/(?P<token>.*)$',
+            password_reset_confirm,
+            {'template_name': 'give/reset_password_confirm.html'},
+            name='password_reset_confirm',
+        ),
+    url(
+            '^reset_password/complete$',
+            password_reset_complete,
+            {'template_name': 'give/reset_password_complete.html'}
+        ),
     url(r'^register$','register'),
     url(r'^unregister$','unregister'),
     url(r'^users$', 'users', name='users'),

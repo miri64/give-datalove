b89fd95924d239c16c399a9925c7cabdafed7177
Authmillenon <authmill@datalove.me>
2012-03-04 01:58:10 +0100

    [give/urls.py] Clean up urls.py a bit

--- datalove/give/urls.py
+++ datalove/give/urls.py
@@ -2,30 +2,28 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login,logout
 
 urlpatterns = patterns('datalove.give.views.html',
-    url('^$','index'),
+    url(r'^$','index'),
     url(    
-            '^login$',
+            r'^login$',
             login,
-            {   
-                'template_name': 'give/welcome.html'
-            },
+            {'template_name': 'give/welcome.html'},
             name='login'
         ),
-    url('^logout$',logout,{'next_page': '/'}),
-    url('^manage_account$','manage_account',name='manage_account'),
-    url('^register$','register'),
-    url('^users$', 'users', name='users'),
+    url(r'^logout$',logout,{'next_page': '/'}),
+    url(r'^manage_account$','manage_account',name='manage_account'),
+    url(r'^register$','register'),
+    url(r'^users$', 'users', name='users'),
     url(
-            '^users/give_(?P<username>[^?$/\\#%\s]+)_datalove$',
+            r'^users/give_(?P<username>[^?$/\\#%\s]+)_datalove$',
             'give_datalove',
             {'from_users': True},
         ),
-    url('^users/(?P<username>[^?$/\\#%\s]+)$','profile',name='profile'),
-    url('^users/(?P<username>[^?$/\\#%\s]+)/give_datalove$','give_datalove'),
-    url('^widget$','widget',name='widget'),
+    url(r'^users/(?P<username>[^?$/\\#%\s]+)$','profile',name='profile'),
+    url(r'^users/(?P<username>[^?$/\\#%\s]+)/give_datalove$','give_datalove'),
+    url(r'^widget$','widget',name='widget'),
     url(
-            '^widget/give_(?P<username>[^?$/\\#%\s]+)_datalove$',
+            r'^widget/give_(?P<username>[^?$/\\#%\s]+)_datalove$',
             'widget_give_datalove',
         ),
-    url('^api/',include('datalove.give.apiurls')),
+    url(r'^api/',include('datalove.give.apiurls')),
 )

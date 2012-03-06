925d95e8d4781274b169959a21af2d8e5cf5bb10
Authmillenon <martin@ucsmail.de>
2012-03-06 21:20:12 +0100

    [give/views/api.py] Define apicall decorator to determine format

--- datalove/give/apiurls.py
+++ datalove/give/apiurls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('datalove.give.views.api',
     url('^$', 'api_doc'),
     url('^history$', 'get_history', name='api__get_history'),
     url('^(?P<format>(json|xml))/history$', 'get_history'),
-    url('^user/(?P<user_id>\d+)$', 'profile', name='api__profile'),
-    url('^(?P<format>(json|xml))/user/(?P<user_id>\d+)$', 'profile'),
+    url('^users/(?P<username>[^?$/\\#%\s]+)$', 'profile', name='api__profile'),
+    url('^(?P<format>[a-z]+)/users/(?P<username>[^?$/\\#%\s]+)$', 'profile'),
 )
--- datalove/give/views/api.py
+++ datalove/give/views/api.py
@@ -4,22 +4,41 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, get_list_or_404
 from give.models import *
 
+from collections import namedtuple
+
 import simplejson as json
 
 from _common import render_to_response2
 
 API_LOGIN_URL=settings.LOGIN_URL
 
-_resp_encode = json.dumps
-_resp_mimetype = 'application/json'
-_req_decode = json.loads
+_FORMAT_LIST = ['json']
 
-def _set_format(format):
-    if format not in ('json'):
-        raise Http404
-    pass
+class _apicall(object):
+    def __init__(self):
+        self.apiview = None
 
-def respond(
+    def set_format(self, *args, **kwargs):
+        Format = namedtuple('Format', 'resp_encode resp_mimetype req_decode')
+        if 'format' != self.apiview.func_code.co_varnames[0]:
+            raise ValueError("'%s' is no apicall. Needs argument 'format' as first argument." % \
+                    self.abiview.__name__)
+        if 'format' in kwargs:
+            format = kwargs.pop('format')
+        else:
+            format = 'json'
+        if format not in _FORMAT_LIST:
+            raise Http404
+        if format == 'json':
+            form = Format(json.dumps, 'application/json', json.loads)
+        args = tuple([form] + list(args))
+        return self.apiview(*args,**kwargs)
+
+    def __call__(self, apiview):
+        self.apiview = apiview
+        return self.set_format
+
+def _respond(
         ResponseObjectType=HttpResponse, 
         content=None, 
         mimetype=None,
@@ -35,24 +54,22 @@ def api_doc(request):
     return render_to_response2(request, 'give/api.html')
 
 @login_required(login_url=API_LOGIN_URL)
-def get_history(request, format='json'):
-    _set_format(format)
+@_apicall()
+def get_history(format, request):
     history = {}
     profile = get_object_or_404(DataloveProfile, pk=request.user.id)
     #history['send'] = common.get_history(sender=profile)
     #history['received'] = common.get_history(recipient=profile)
-    return respond(
-            content=_resp_encode(history)
+    return _respond(
+            content=format.resp_encode(history)
         )
 
-def profile(request, user_id, format='json'):
-    _set_format(format)
-    profile = get_object_or_404(DataloveProfile,pk=user_id)
+@_apicall()
+def profile(format, request, username):
+    profile = get_object_or_404(DataloveProfile,user__username=username)
     selection = ['username','received_love','websites']
     if request.user.is_authenticated() and request.user == profile.user:
         selection += ['available_love']
-    return respond(
-                content=_resp_encode(
-                        profile.get_profile_dict(selection)
-                    )
+    return _respond(
+            content=format.resp_encode(profile.get_profile_dict(selection))
         )

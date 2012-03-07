96fa2c2de49f334d3755c9b9123f751022f4b29e
Authmillenon <authmill@datalove.me>
2012-03-07 18:08:20 +0100

    [give/apiurls.py] Make patterns to raw strings

--- datalove/give/apiurls.py
+++ datalove/give/apiurls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 import views
 
 urlpatterns = patterns('datalove.give.views.api',
-    url('^$', 'api_doc'),
-    url('^history$', 'get_history', name='api__get_history'),
-    url('^(?P<format>(json|xml))/history$', 'get_history'),
-    url('^users/(?P<username>[^?$/\\#%\s]+)$', 'profile', name='api__profile'),
-    url('^(?P<format>[a-z]+)/users/(?P<username>[^?$/\\#%\s]+)$', 'profile'),
+    url(r'^$', 'api_doc'),
+    url(r'^history$', 'get_history', name='api__get_history'),
+    url(r'^(?P<format>(json|xml))/history$', 'get_history'),
+    url(r'^users/(?P<username>[^?$/\\#%\s]+)$', 'profile', name='api__profile'),
+    url(r'^(?P<format>[a-z]+)/users/(?P<username>[^?$/\\#%\s]+)$', 'profile'),
 )

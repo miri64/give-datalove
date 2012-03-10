fb63f44b17d3055de085af982b6f8feb135163be
Authmillenon <authmill@datalove.me>
2012-03-10 20:40:13 +0100

    Fixed URL regexes

--- datalove/give/urls.py
+++ datalove/give/urls.py
@@ -4,10 +4,10 @@ from django.contrib.auth.views import login, logout, password_reset, \
 from give.forms import DataloveAuthenticationForm
 
 urlpatterns = patterns('datalove.give.views.html',
-    url(r'^$','index'),
-    url(r'^history$','history'),
+    url(r'^/?$','index'),
+    url(r'^history/?$','history'),
     url(    
-            r'^login$',
+            r'^login/?$',
             login,
             {
                 'template_name': 'give/welcome.html',
@@ -15,10 +15,10 @@ urlpatterns = patterns('datalove.give.views.html',
             },
             name='login'
         ),
-    url(r'^logout$',logout,{'next_page': '/'}),
-    url(r'^manage_account$','manage_account',name='manage_account'),
+    url(r'^logout/?$',logout,{'next_page': '/'}),
+    url(r'^manage_account/?$','manage_account',name='manage_account'),
     url(
-            r'^reset_password$',
+            r'^reset_password/?$',
             password_reset,
             {
                 'template_name': 'give/reset_password.html',
@@ -28,34 +28,34 @@ urlpatterns = patterns('datalove.give.views.html',
             name='password_reset'
         ),
     url(
-            '^reset_password/done$',
+            '^reset_password/done/?$',
             password_reset_done,
             {'template_name': 'give/reset_password_done.html'}
         ),
     url(
-            r'^reset_password/confirm/(?P<uidb36>[0-9]+)/(?P<token>[-0-9a-f]+)$',
+            r'^reset_password/confirm/(?P<uidb36>[0-9]+)/(?P<token>[-0-9a-f]+)/?$',
             password_reset_confirm,
             {'template_name': 'give/reset_password_confirm.html'},
             name='password_reset_confirm',
         ),
     url(
-            '^reset_password/complete$',
+            '^reset_password/complete/?$',
             password_reset_complete,
             {'template_name': 'give/reset_password_complete.html'}
         ),
-    url(r'^register$','register',name='register'),
-    url(r'^unregister$','unregister'),
-    url(r'^users$', 'users', name='users'),
+    url(r'^register/?$','register',name='register'),
+    url(r'^unregister/?$','unregister'),
+    url(r'^users/?$', 'users', name='users'),
     url(
-            r'^users/give_(?P<username>[^?$/\\#%\s]+)_datalove$',
+            r'^users/give_(?P<username>[^?$/\\#%\s]+)_datalove/?$',
             'give_datalove',
             {'from_users': True},
         ),
-    url(r'^users/(?P<username>[^?$/\\#%\s]+)$','profile',name='profile'),
-    url(r'^users/(?P<username>[^?$/\\#%\s]+)/give_datalove$','give_datalove'),
-    url(r'^widget$','widget',name='widget'),
+    url(r'^users/(?P<username>[^?$/\\#%\s]+)/?$','profile',name='profile'),
+    url(r'^users/(?P<username>[^?$/\\#%\s]+)/give_datalove/?$','give_datalove'),
+    url(r'^widget/?$','widget',name='widget'),
     url(
-            r'^widget/give_(?P<username>[^?$/\\#%\s]+)_datalove$',
+            r'^widget/give_(?P<username>[^?$/\\#%\s]+)_datalove/?$',
             'widget_give_datalove',
         ),
     url(r'^api/',include('datalove.give.apiurls')),

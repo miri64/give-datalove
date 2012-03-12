eca7d26d2df9d6c1b582dd59bc16452369484bd7
Authmillenon <authmill@datalove.me>
2012-03-12 02:17:53 +0100

    More trouble with the submodule

--- settings.py
+++ settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'give',
-    'piston',
+    'django-piston.piston',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

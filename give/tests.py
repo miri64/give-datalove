b35bed6c18b3c864c4b80a9965ac6bd53c1f5d0a
Authmillenon <authmill@datalove.me>
2012-03-12 03:59:05 +0100

    Create index test case

--- give/tests.py
+++ give/tests.py
@@ -5,12 +5,45 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
+import views
+from models import create_user
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username, self.user = create_user()
+
+    def test_index_get_without_login(self):
+        response = self.client.get(reverse(views.index))
+        self.assertEqual(200,response.status_code)
+        self.assertIn('next',  response.context)
+        self.assertIn('form',  response.context)
+        self.assertTemplateUsed(response,'give/welcome.html')
+
+    def test_index_get_login(self):
+        self.assertTrue(
+                self.client.login(
+                        username=self.username, 
+                        password=self.username
+                    )
+            )
+        response = self.client.get(reverse(views.index))
+        self.assertEqual(200,response.status_code)
+        if 'next' in response.context:
+            self.assertIsNone(response.context['next'])
+        self.assertTemplateUsed(response,'give/userpage.html')
+    
+    def test_index_post_no_data(self):
+        response = self.client.post(reverse(views.index))
+        self.assertEqual(200,response.status_code)
+        self.assertTemplateUsed(response,'give/welcome.html')
+        self.assertIn('next',  response.context)
+        self.assertIn('form',  response.context)
+        form = response.context['form']
+        self.assertContains(response,'This field is required.')
+
+    def tearDown(self):
+        self.user.delete()

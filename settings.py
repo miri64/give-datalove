14f18ea87c723e15e346a63136fdbf1962411ebc
Authmillenon <authmill@datalove.me>
2012-03-16 18:03:39 +0100

    Forgot it ...

--- settings.py
+++ settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'give',
-    'django-piston.piston',
+    'djangopiston.piston',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

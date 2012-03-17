ca96db63fd760a81a60b8bcf37c4df5c4d4a3695
authmillenon <authmill@datalove.me>
2012-03-17 18:03:58 +0100

    Put context handling to _common.py

--- give/views/__init__.py
+++ give/views/__init__.py
@@ -1,15 +1,10 @@
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.views import login
-from django.core.urlresolvers import reverse
-from django.db import IntegrityError
-from django.http import HttpResponse, Http404, HttpResponseBadRequest
-from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseBadRequest
+from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_protect
 from urllib import urlencode
-from give.forms import *
-from give.models import *
+from give.models import DataloveProfile
 import _common as common
 
 ## Helper functions
@@ -42,82 +37,45 @@ def index(request):
                 authentication_form=DataloveAuthenticationForm
             )
 
-
 @login_required
 def history(request,username):
     if request.user.username != username:
         return redirect('profile', username)
-    profile = request.user.get_profile()
-    vars = {}
-    vars['received'] = profile.receive_history.all()[:30]
-    vars['sent'] = profile.send_history.all()[:30]
-    return common.render_to_response2(request,'give/history.html',vars)
-
+    context = common.get_history_context(request)
+    return common.render_to_response2(request,'give/history.html',context)
 
 @login_required
 @csrf_protect
 def manage_account(request):
-    user_form = DataloveUserChangeForm(instance=request.user,prefix="user")
-    password_form = PasswordChangeForm(
-            request.user,
-            prefix="password"
-        )
-    profile_form = UserWebsiteFormSet(
-            request.user,
-            prefix="profile"
-        )
+    context = common.get_manage_account_context(request)
     if request.method == 'POST':
+        form_valid = False
         if "user" in request.POST:
-            user_form = DataloveUserChangeForm(
-                    request.POST,
-                    instance=request.user,
-                    prefix="user"
-                )
-            if user_form.is_valid():
-                user_form.save()
-                return redirect(manage_account)
-        if "password" in request.POST:
-            password_form = PasswordChangeForm(
-                    request.user,
-                    request.POST,
-                    prefix="password"
-                )
-            if password_form.is_valid():
-                password_form.save()
-                return redirect(manage_account)
-        if "profile" in request.POST:
-            profile_form = UserWebsiteFormSet(
-                    request.user,
-                    request.POST,
-                    prefix="profile"
-                )
-            if profile_form.is_valid():
-                profile_form.save()
-                return redirect(manage_account)
+            context, form_valid = common.check_user_form(request, context) 
+        elif "password" in request.POST:
+            context, form_valid = common.check_password_form(request, context) 
+        elif "profile" in request.POST:
+            context, form_valid = common.check_profile_form(request, context) 
+        if form_valid:
+            return redirect(manage_account)
     return common.render_to_response2(
             request,
             'give/manage_account.html',
-            {
-                'user_form': user_form,
-                'password_form': password_form,
-                'profile_form': profile_form
-            }
+            context
         )
 
 def users(request):
-    profiles = DataloveProfile.objects.order_by('?')
-    vars = common.get_more_information(request, {'profiles': profiles})
-    return common.render_to_response2(request,'give/users.html',vars)
+    context = common.get_users_context(request)
+    return common.render_to_response2(request,'give/users.html',context)
 
 @csrf_protect
 def register(request):
-    form = DataloveUserCreationForm()
+    context = common.get_register_context(request)
     if request.method == 'POST':
-        form = DataloveUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+        context, form_valid = common.check_register_form(request,context)
+        if form_valid:
             return redirect(index)
-    return common.render_to_response2(request,'give/register.html',{'form':form})    
+    return common.render_to_response2(request,'give/register.html',context)    
 
 @login_required
 def unregister(request):
@@ -125,9 +83,8 @@ def unregister(request):
     return redirect(index)
 
 def profile(request, username):
-    profile = get_object_or_404(DataloveProfile, user__username=username)
-    vars = common.get_more_information(request, {'profile': profile})
-    return common.render_to_response2(request,'give/profile.html',vars)
+    context = common.get_profile_context(request,username)
+    return common.render_to_response2(request,'give/profile.html',context)
 
 @login_required
 def give_datalove(request, username, from_users=False):
@@ -138,16 +95,16 @@ def give_datalove(request, username, from_users=False):
         return query_redirect(profile, query, username)
 
 def widget(request):
-    vars = {'error': request.GET['error']} if 'error' in request.GET else {}
+    context = {'error': request.GET['error']} if 'error' in request.GET else {}
     if 'random' in request.GET and 'user' not in request.GET:
-        vars['profile'] = DataloveProfile.get_random_profile()
+        context['profile'] = DataloveProfile.get_random_profile()
     elif 'user' in request.GET and 'random' not in request.GET:
         try:
-            vars['profile'] = DataloveProfile.objects.get(
+            context['profile'] = DataloveProfile.objects.get(
                     user__username=request.GET['user']
                 )
         except DataloveProfile.DoesNotExist:
-            vars['error'] = "User '%s' does not exist" % request.GET['user']
+            context['error'] = "User '%s' does not exist" % request.GET['user']
     else:
         return HttpResponseBadRequest(
                 "GET request must have eather query parameter 'user' or "
@@ -156,11 +113,8 @@ def widget(request):
     return common.render_to_response2(request, 'give/widget.html', context)
 
 def widget_doc(request):
-    context = {}
-    if request.user.is_authenticated():
-        context['profile'] = request.user.get_profile()
+    context = common.get_widget_doc_context(request)
     return common.render_to_response2(request, 'give/widget_doc.html', context)
-
 
 def widget_give_datalove(request, username):
     if not request.user.is_authenticated():
--- give/views/_common.py
+++ give/views/_common.py
@@ -1,8 +1,11 @@
+import re
+
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-from give.models import DataloveProfile, NotEnoughDataloveException, DataNarcismException, LovableObject
 
-import re
+from give.models import *
+from give.forms import *
 
 def render_to_response2(request, *args, **kwargs):
     if 'context_instance' in kwargs:
@@ -16,9 +19,11 @@ def render_to_response2(request, *args, **kwargs):
         )
 
 def give_datalove(request, id, love=1, query={}):
+    if not request.user.is_authenticated():
+        raise UserException("You are not logged in.")
     if re.match(r"[0-9]+",id):
         recipient = get_object_or_404(LovableObject, id=id)
-    elif re.match(r"[^?$/\\#%\s]+"):
+    elif re.match(r"[^?$/\\#%\s]+",id):
         recipient = get_object_or_404(DataloveProfile, user__username=id)
     else:
         recipient = id
@@ -40,3 +45,81 @@ def get_more_information(request, vars={}):
         vars.update({'error': request.GET['error']})
     return vars
 
+def check_form(form):
+    if form.is_valid():
+        form.save()
+        return True
+    return False
+
+def get_history_context(request):
+    profile = request.user.get_profile()
+    context = {}
+    context['received'] = profile.receive_history.all()[:30]
+    context['sent'] = profile.send_history.all()[:30]
+    return context 
+
+def get_manage_account_context(request):
+    context = {}
+    context['user_form'] = DataloveUserChangeForm(
+            instance=request.user,
+            prefix="user"
+        )
+    context['password_form'] = PasswordChangeForm(
+            request.user,
+            prefix="password"
+        )
+    context['profile_form'] = UserWebsiteFormSet(
+            request.user,
+            prefix="profile"
+        )
+    return context
+
+def check_user_form(request, context):
+    context['user_form'] = DataloveUserChangeForm(
+            request.POST,
+            instance=request.user,
+            prefix="user"
+        )
+    return context, check_form(context['user_form'])
+
+def check_password_form(request, context):
+    context['password_form'] = PasswordChangeForm(
+            request.user,
+            request.POST,
+            prefix="user"
+        )
+    return context, check_form(context['password_form'])
+
+def check_profile_form(request, context):
+    context['profile_form'] = UserWebsiteFormSet(
+            request.user,
+            request.POST,
+            prefix="profile"
+        )
+    return context, check_form(context['profile_form'])
+
+def get_register_context(request):
+    context = {}
+    context['form'] = DataloveUserCreationForm()
+    return context
+
+def check_register_form(request, context):
+    context['form'] = DataloveUserCreationForm(request.POST)
+    return context, check_form(context['form'])
+
+def get_users_context(request):
+    profiles = DataloveProfile.objects.order_by('?')
+    context = get_more_information(request, {'profiles': profiles})
+    return context
+
+def get_profile_context(request, username):
+    profile = get_object_or_404(DataloveProfile, user__username=username)
+    context = get_more_information(request, {'profile': profile})
+    return context
+
+def get_widget_doc_context(request):
+    context = {}
+    if request.user.is_authenticated():
+        context['profile'] = request.user.get_profile()
+    return context
+    
